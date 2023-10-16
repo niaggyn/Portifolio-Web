d166b08081cf2c6a5075fd480f56a01fcecd4de6
Niaggy Novoa <133553719+niaggyn@users.noreply.github.com>
2023-10-16 10:12:33 -0300

    Chnages

--- src/main.jsx
+++ src/main.jsx
@@ -11,3 +11,12 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
+
+/* const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals(); */
